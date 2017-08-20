87fecaf9cd3ea6b661db694a99879d0706e664d7
minakawa <minakawa.daiki.as@gmail.com>
2017-08-20 21:08:22 +0900

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
   end
+  def any
+    redirect_to action: index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -50,4 +50,5 @@ Rails.application.routes.draw do
     end
 
   end
+  match '*any' => 'home#any', via: :all
 end
